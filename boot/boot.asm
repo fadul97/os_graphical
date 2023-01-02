bbce81f9c378f618788289049983ac66641ff386
Leonardo Sousa Ferreira Fadul <lsffadul@gmail.com>
2023-01-02 13:28:20 -0300

    Clear screen

--- boot/boot.asm
+++ boot/boot.asm
@@ -3,10 +3,22 @@
 
 section code
 
-.main:
+.init:
 	mov eax, 0xb800
 	mov es, eax
+	mov eax, 0 ; Set eax to 0 -> i = 0
 
+.clear:
+	mov byte [es:eax], 0 ; Move black character to current text address
+	inc eax;
+	mov byte [es:eax], 0x30 ; Move background color and character color to the next address
+	inc eax;
+
+	cmp eax, 2 * 25 * 80
+
+	jl .clear
+
+.main:
 	mov byte [es:0x00], 'H' ; 0xb800 + 0x00 = 0xb800
 	mov byte [es:0x01], 0x30
 
