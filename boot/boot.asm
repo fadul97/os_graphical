30e1f09d519cebf41053dd110ea85b826e73057d
Leonardo Fadul <lsffadul@gmail.com>
2023-01-20 17:49:10 -0300

    Making a print utility

--- boot/boot.asm
+++ boot/boot.asm
@@ -4,9 +4,14 @@
 section code
 
 .init:
+	mov eax, 0x07c0
+	mov ds, eax
 	mov eax, 0xb800
 	mov es, eax
 	mov eax, 0 ; Set eax to 0 -> i = 0
+	mov ebx, 0 ; Index of character in the string we are printing
+	mov ecx, 0 ; Actual address of the character on the screen
+	mov dl, 0 ; Store actual value that we are printing to the screen
 
 .clear:
 	mov byte [es:eax], 0 ; Move black character to current text address
@@ -18,11 +23,31 @@ section code
 
 	jl .clear
 
-.main:
-	mov byte [es:0x00], 'H' ; 0xb800 + 0x00 = 0xb800
-	mov byte [es:0x01], 0x30
+mov eax, text1
+push .print
 
-jmp $
+.print:
+	mov dl, byte [eax + ebx]
+
+	cmp dl, 0
+	je .print_end
+
+	mov byte [es:ecx], dl
+
+	inc ebx
+	inc ecx
+	inc ecx
+
+	jmp .print
+
+.print_end:
+	ret
+
+.end:
+	jmp $
+
+text: db 'Hello, World!', 0
+text1: db 'This is another text!', 0
 
 times 510 - ($ - $$) db 0x00 ; Pads file with 0s, making it the right size
 
