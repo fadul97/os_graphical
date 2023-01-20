36fa74219c72808966b4aae76524cc24ecb22d6b
Leonardo Fadul <lsffadul@gmail.com>
2023-01-20 18:19:46 -0300

    Switching to protected mode

--- boot/boot.asm
+++ boot/boot.asm
@@ -1,11 +1,9 @@
-[org 0x00]
+[org 0x7c00]
 [bits 16]
 
 section code
 
 .init:
-	mov eax, 0x07c0
-	mov ds, eax
 	mov eax, 0xb800
 	mov es, eax
 	mov eax, 0 ; Set eax to 0 -> i = 0
@@ -23,11 +21,12 @@ section code
 
 	jl .clear
 
-mov eax, text1
-mov ecx, 3 * 2 * 80
-push .end
+mov eax, welcome
+mov ecx, 0 * 2 * 80
 
 call .print
+
+jmp .switch
 
 .print:
 	mov dl, byte [eax + ebx]
@@ -46,12 +45,59 @@ call .print
 .print_end:
 	ret
 
-.end:
-	mov byte [es:0x00], 'L' ; FIXME: Not printing L on the first line
+.switch:
+	cli ; Turn off interrupts
+	lgdt [gdt_descriptor] ; Load GDT Table
+
+	mov eax, cr0
+	or eax, 0x1
+	mov cr0, eax ; Make the switch
+
+	jmp protected_start
+
+welcome: db 'Welcome to GraphicalOS!', 0
+
+[bits 32]
+protected_start:
+	mov ax, data_seg
+	mov ds, ax
+	mov ss, ax
+	mov es, ax
+	mov fs, ax
+	mov gs, ax
+
+	; Update stack pointer
+	mov ebp, 0x90000
+	mov esp, ebp
+
 	jmp $
 
-text: db 'Hello, World!', 0
-text1: db 'This is another text!', 0
+gdt_begin:
+gdt_null_descriptor:
+	dd 0x00
+	dd 0x00
+gdt_code_seg:
+	dw 0xffff
+	dw 0x00
+	db 0x00
+	db 10011010b
+	db 11001111b
+	db 0x00
+gdt_data_seg:
+	dw 0xffff
+	dw 0x00
+	db 0x00
+	db 10010010b
+	db 11001111b
+	db 0x00
+gdt_end:
+gdt_descriptor:
+	dw gdt_end - gdt_begin - 1
+	dd gdt_begin
+
+code_seg equ gdt_code_seg - gdt_begin
+data_seg equ gdt_data_seg - gdt_begin
+	 
 
 times 510 - ($ - $$) db 0x00 ; Pads file with 0s, making it the right size
 
